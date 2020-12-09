3cc6b57b4e1bd87293342f89b5931f17e7ae4211
EDSEL <ealvan@unsa.edu.pe>
2020-12-08 22:58:07 -0500

    creando fase intermeda del projecto, falta depurar y mejorar el modo de recomendaciones del algoritmo

--- Merge_and_Count/CountingInversions2.java
+++ Merge_and_Count/CountingInversions2.java
@@ -3,13 +3,129 @@ import java.util.ArrayList;
 import java.util.Arrays; 
 public class CountingInversions2 {
 	  
-	
-	    // Function to count the number of inversions 
+		String[] turisticos = {
+				"0",
+				"1",
+				"2",
+				"3",
+				"4",
+				"5",
+				"6",
+				"7",
+				"8",
+				"9",
+				"10",
+				"11",
+				"12",
+				"13"
+		};
+		int[] road1 = {0,1,2,3,4,5,6,7,8,9,10,11,13};
+		
+		int[] road2 = {0,6,7,1,4,10,11,13,2,3,5,8,9 };
+		int[] road3 = {0,1,2,3,4,5,6,7,8,9,10,11,13};
+		
+		int[] road4 = {0,6,7,1,4,10,11,13,2,3,5,8,9 };
+		int[][] roadList = {
+				road1,
+				road2,
+				road3,
+				road4, 	
+		};
+	 
+		// Function to count the number of inversions 
+		
 	    // during the merge process 
-	    private static int mergeAndCount(int[] arr,  
-	                                int l, int m, int r) 
-	    { 
-	  
+	    
+	   
+	   
+	     
+	 
+	   /*
+	    *  public static int[] ordenarIds(empaque[] nums) {
+	    	for(int i = 1; i < nums.length; i++) {
+	    		int pos = nums[i].pos;
+	    		int j = i-1;
+	    		while(j >= 0 && nums[j].pos > pos ) {
+	    			nums[j+1] = nums[j];
+	    			j --;
+	    		}
+	    		nums[j+1]= nums[i];
+	    	}
+	    	int[] list = new int[nums.length];
+	    	int k  =0;
+	    	for(empaque key: nums) {
+	    		list[k] = key.value; 
+	    		k++;
+	    	}
+	    	return list;
+	    }
+	    * */
+		public ArrayList<Integer> toArrayList(int[] integer){
+			ArrayList<Integer> lista = new ArrayList<Integer>();
+			for(int k:integer) {
+				lista.add(k);
+			}
+			return lista;
+				
+		}
+	    //*************************************************************************
+	    public   void main_second(Ciudad[] lugares) {
+	    	int items[] = new int[lugares.length];
+	    	for(int i = 0; i < items.length; i++) {
+	    		items[i] = lugares[i].id;
+	    	}
+	    	int[] keys = null;
+	    	int[] inversions = new int[keys.length];
+	    	for(int i = 0; i < roadList.length; i++) {
+	    		keys = cortarGen(roadList[i],items);
+	    		inversions[i] = mergeSortAndCount(keys,0,keys.length);
+	    	}
+	    	int isUserInversions = mergeSortAndCount(items,0,items.length);
+	    	int idNear = id_list_menor(inversions,isUserInversions);
+	    	int[] nearRoad = roadList[idNear];
+	    	ArrayList<Integer> nearR = toArrayList(nearRoad);
+	    	ArrayList<Integer> complement = complement(nearR, items);
+	    	
+	    }
+	    ///************************************************************************
+	    // los lugares, aqui se muestran
+	    // debo cortarlos
+	    public static int[] cortarGen(int origin[], int[] items) {
+	    	empaque[] m = fase1_cortar(origin, items);
+	    	ordenarIds(m);
+	    	int[] keys = new int[m.length];
+	    	for(int i = 0; i < m.length; i++) {
+	    		keys[i] = m[i].value;
+	    	}
+	    	return keys;
+	    }
+	     
+	    public static empaque[] fase1_cortar(int[] original, int[] items) {
+	    	empaque[] numsCortados = new empaque[items.length];
+	    	int j = 0;
+	    	for(int key: items)
+	    	for(int i = 0; i < original.length; i++) {
+	    		if(key == original[i]) {
+	    			numsCortados[j] = new empaque(i,key);
+	    			j++;
+	    		}
+	    	}
+	    	return numsCortados;
+	    }
+	    public static void ordenarIds(empaque arr[]){ 
+	         int n = arr.length; 
+	         for (int i = 0; i < n-1; i++) 
+	             for (int j = 0; j < n-i-1; j++) 
+	                 if (arr[j].pos > arr[j+1].pos){ 
+	                        // swap arr[j+1] and arr[j] 
+	                        empaque temp = arr[j]; 
+	                        arr[j] = arr[j+1]; 
+	                        arr[j+1] = temp; 
+	                    } 
+	    } 
+	   // *************************************************
+	    private static int mergeAndCount(int[] arr,int l, int m, int r){ 
+	  	  
 	        // Left subarray 
 	        int[] left = Arrays.copyOfRange(arr, l, m + 1); 
 	  
@@ -31,9 +147,7 @@ public class CountingInversions2 {
 	    } 
 	  
 	    // Merge sort function 
-	    private static int mergeSortAndCount(int[] arr,  
-	                                        int l, int r) 
-	    { 
+	    private static int mergeSortAndCount(int[] arr, int l, int r){ 
 	  
 	        // Keeps track of the inversion count at a 
 	        // particular node of the recursion tree 
@@ -57,6 +171,7 @@ public class CountingInversions2 {
 	  
 	        return count; 
 	    } 
+	    //***************************************************************
 	    public static int id_list_menor(int[]inversions, int user_inv) {
 	    	int menor = Math.abs(inversions[0] - user_inv);
 	    	int id_list = -1;
@@ -68,6 +183,8 @@ public class CountingInversions2 {
 	    	}
 	    	return id_list;
 	    }
+	   // *****************************************************************
+	    
 	    public static ArrayList<Integer> complement(ArrayList<Integer> original, int[] quitar) {
 	    	
 	    	for(int key: quitar) {
@@ -79,41 +196,7 @@ public class CountingInversions2 {
 	    	}
 	    	return original;
 	    }
-	    public static empaque[] fase1_cortar(int[] original, int[] items) {
-	    	empaque[] numsCortados = new empaque[items.length];
-	    	int j = 0;
-	    	for(int key: items)
-	    	for(int i = 0; i < original.length; i++) {
-	    		if(key == original[i]) {
-	    			numsCortados[j] = new empaque(i,key);
-	    			j++;
-	    		}
-	    	}
-	    	return numsCortados;
-	    }
-	    public static int[] ordenarIds(empaque[] nums) {
-	    	for(int i = 1; i < nums.length; i++) {
-	    		int pos = nums[i].pos;
-	    		int j = i-1;
-	    		while(j >= 0 && nums[j].pos > pos ) {
-	    			nums[j+1] = nums[j];
-	    			j --;
-	    		}
-	    		nums[j+1].pos = pos;
-	    	}
-	    	int[] list = new int[nums.length];
-	    	int k  =0;
-	    	for(empaque key: nums) {
-	    		list[k] = key.value; 
-	    		k++;
-	    	}
-	    	return list;
-	    }
-	    
-	    
-	    // Driver code 
-	    public static void main(String[] args) 
-	    { 
+	    public static void main(String[] args) { 
 			//int[]list = {1,20,6,4,5};
 	        //System.out.println(mergeSortAndCount(list, 0, list.length - 1)); 
 	    	int[] list_1 = {1,8,4,5,3,1,21,2};
@@ -129,8 +212,8 @@ public class CountingInversions2 {
 	    	for(empaque key: pack) {
 	    		System.out.println(key);
 	    	}
-	    	int[]ids = ordenarIds(pack);
-	    	System.out.println(Arrays.toString(ids));
+	    	 ordenarIds(pack);
+	    	System.out.println(Arrays.toString(pack));
 	    	
 	    	
 	    	
